5c4a8997d868f8806a481cf93d97f9f32c0b1835
kikito <kikito@gmail.com>
2013-05-01 17:49:26 +0200

    added basic layout and font style

--- config.rb
+++ config.rb
@@ -7,9 +7,9 @@
 # require 'susy'
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+#  config.output_style = :compressed
+end
 
 ###
 # Page options, layouts, aliases and proxies
